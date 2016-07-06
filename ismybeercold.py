a58b273f454f0bb99b96593bfab136c8ec817a1c
Gene Blanchard <gblanchard4@gmail.com>
2016-07-06 18:55:34 -0500

    jsonify

--- ismybeercold.py
+++ ismybeercold.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 from flask import Flask, render_template, request, jsonify
 import datetime
+import psutil
 from subprocess import check_output
 import thermometer
 import logging
@@ -12,16 +13,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def ismybeercold():
+    return render_template('index.html', title="ISMYBEERCOLD?")
+
+
+@app.route("_jsondata")
+def jsondata():
     uptime = getUptime()
     tempString = thermometer.read_temp()
     timeString = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-    templateData = {
-        'title': 'ISMYBEERCOLD?',
-        'time': timeString,
-        'temp': tempString,
-        'up': uptime
-    }
-    return render_template('index.html', **templateData)
+    cpu_percent = psutil.cpu_percent()
+    virtmem_percent = psutil.virtual_memory()[2]
+    return jsonify(uptime=uptime, temp=tempString, time=timeString, cpu=cpu_percent, ram=virtmem_percent)
 
 
 def getUptime():
@@ -29,6 +31,6 @@ def getUptime():
     uptime = output[output.find("up"):output.find("user") - 5]
     return uptime
 
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    # app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8888, debug=True)
