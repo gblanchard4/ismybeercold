f4ec96cfb4fd58bd3928568741c0f44616b6aeff
Gene Blanchard <gblanchard4@gmail.com>
2017-06-04 22:10:32 -0500

    modprobe

--- src/thermometer.py
+++ src/thermometer.py
@@ -3,6 +3,9 @@ import glob
 import os
 import time
 
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
