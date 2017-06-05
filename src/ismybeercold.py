d1ac5644005b6805f04e1e299b5359a97ea49759
Gene Blanchard <gblanchard4@gmail.com>
2017-06-04 21:51:31 -0500

    move to port 80

--- src/ismybeercold.py
+++ src/ismybeercold.py
@@ -36,5 +36,4 @@ def getUptime():
     return uptime
 
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0', port=8080, debug=True)
-    app.run(host='0.0.0.0', port=8888, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
