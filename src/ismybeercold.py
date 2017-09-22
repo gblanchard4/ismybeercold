5480a1333bb96c32ea634149190a226df1b1e703
Gene Blanchard <gblanchard4@gmail.com>
2017-09-22 01:55:04 -0600

    schedule dis

--- src/ismybeercold.py
+++ src/ismybeercold.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
 import datetime
-import time
+import atexit
 import psutil
 import logging
 import os
 from flask import Flask, render_template, request, jsonify
-from flask_script import Manager
 from w1thermsensor import W1ThermSensor
 from subprocess import check_output
 from datadog import initialize, api
@@ -29,6 +28,17 @@ os.system('modprobe w1-therm')
 sensor = W1ThermSensor()
 temperature = None
 
+scheduler = BackgroundScheduler()
+scheduler.start()
+scheduler.add_job(
+    func=dd_temp_update,
+    trigger=IntervalTrigger(seconds=5),
+    id='dd_temp_update',
+    name='Update temperature on DD every five seconds',
+    replace_existing=True)
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
+
 def read_temp():
     temperature = sensor.get_temperature(W1ThermSensor.DEGREES_F)
     return temperature
@@ -41,15 +51,14 @@ def getUptime():
 
 
 def dd_temp_update():
-    while True:
-        api.Metric.send(metric='jeferaptor.temperature', points="{0:.2f}".format(read_temp()), type='counter', host='Jeferaptor')
-        time.sleep(10)
+    api.Metric.send(metric='jeferaptor.temperature', points="{0:.2f}".format(read_temp()), type='counter', host='Jeferaptor')
 
 
 @app.route("/")
 def ismybeercold():
     api.Metric.send(metric='jeferaptor.page_views', points=1, type='counter', host='Jeferaptor')
     return render_template('index.html', title="ISMYBEERCOLD?")
+
 
 @app.route("/_jsondata")
 def jsondata():
@@ -64,11 +73,6 @@ def jsondata():
     cpu_percent = psutil.cpu_percent()
     virtmem_percent = psutil.virtual_memory()[2]
     return jsonify(uptime=uptime, temp=tempString, time=timeString, cpu=cpu_percent, ram=virtmem_percent, saying=saying)
-
-@manager.command
-def runserver():
-    app.run()
-    crazy_call()
 
 
 if __name__ == "__main__":
