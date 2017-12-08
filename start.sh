8cea8850188f94eb705935719441193f18b3eee3
Gene Blanchard <gblanchard4@gmail.com>
2017-12-08 16:35:33 -0700

    port conflict

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 python3 src/ismybeercold.py &
-cd /etc/node_exporter-$NODE_EXPORTER_VERSION.$DIST_ARCH \
-  && ./node_exporter --web.listen-address=":80" --web.telemetry-path="/metrics"
+# cd /etc/node_exporter-$NODE_EXPORTER_VERSION.$DIST_ARCH \
+#   && ./node_exporter --web.listen-address=":80" --web.telemetry-path="/metrics"
