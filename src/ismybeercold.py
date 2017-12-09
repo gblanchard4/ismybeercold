f59defbb5c8315a41fc34f0e4bf6c589f3b48e6e
Gene Blanchard <gblanchard4@gmail.com>
2017-12-09 12:13:23 -0700

    add prometheus app moniotor

--- src/ismybeercold.py
+++ src/ismybeercold.py
@@ -4,19 +4,12 @@ import psutil
 import logging
 import os
 from flask import Flask, render_template, request, jsonify
+from flask_prometheus import monitor
 from time import sleep
 from multiprocessing import Process, Value
 from w1thermsensor import W1ThermSensor
 from subprocess import check_output
-from datadog import initialize, api
 from uptime import uptime
-
-dd_options = {
-    'api_key':os.environ['DD_API_KEY'],
-    'app_key':os.environ['DD_APP_KEY'],
-    'hostname':'jeferaptor'
-}
-initialize(**dd_options)
 
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
@@ -29,34 +22,15 @@ os.system('modprobe w1-therm')
 sensor = W1ThermSensor()
 temperature = None
 
-page_views = 0
-
 def read_temp():
     temperature = sensor.get_temperature(W1ThermSensor.DEGREES_F)
     temperature = "{0:.2f}".format(temperature)
     temperature = float(temperature)
     return temperature
 
-
-def dd_temp_update():
-    while True:
-        temperature = read_temp()
-        print("START: Update DD temperature metric =={}".format(temperature))
-        try:
-            api.Metric.send(metric='jeferaptor.temperature', points=temperature, type='counter', host='Jeferaptor')
-            sleep(1)
-        except HttpTimeout:
-            pass
-
-
 @app.route("/")
 def ismybeercold():
-    global page_views
-    page_views += 1
-    api.Metric.send(metric='jeferaptor.page_views', points=page_views, type='counter', host='Jeferaptor')
-    print("page_views incremented")
     return render_template('index.html', title="ISMYBEERCOLD?")
-
 
 @app.route("/_jsondata")
 def jsondata():
@@ -72,10 +46,6 @@ def jsondata():
     virtmem_percent = psutil.virtual_memory()[2]
     return jsonify(uptime=uptime_var, temp=tempString, time=timeString, cpu=cpu_percent, ram=virtmem_percent, saying=saying)
 
-
 if __name__ == "__main__":
-    page_views = 0
-    dd_process = Process(target=dd_temp_update)
-    dd_process.start()
+    monitor(app, port=8000)
     app.run(host='0.0.0.0', port=80, debug=False)
-    dd_process.join()
